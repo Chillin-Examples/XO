1bcb20fdfa8d901dfed4bd6d717c8d5ae1022cbc
AminHP <mdan.hagh@gmail.com>
2019-09-03 18:02:07 +0430

    Minor refactor

--- PythonServer/generate_ks.py
+++ PythonServer/generate_ks.py
@@ -1,12 +1,31 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# python imports
+import os
+from shutil import copyfile
+
+# chillin imports
 from koala_serializer import generate
-import sys
 
-lang = 'python'
-if len(sys.argv) > 1:
-    lang = sys.argv[1]
 
-generate('ks/commands.ks', lang, 'ks')
-generate('ks/models.ks', lang, 'ks')
+commands_reletive_dir = 'ks/commands.ks'
+models_reletive_dir = 'ks/models.ks'
+destinations = [
+    '../PythonClient',
+    '../CppClient/Game'
+]
+
+for dest in destinations:
+    copyfile('ks/commands.ks', os.path.join(dest, commands_reletive_dir))
+    copyfile('ks/models.ks', os.path.join(dest, models_reletive_dir))
+
+all_args = [
+    ('python', 'ks', 'snake_case'),
+    ('python', '../PythonClient/ks', 'snake_case'),
+    ('cpp', '../CppClient/Game/ks', 'camelCase')
+]
+
+for args in all_args:
+    generate('ks/commands.ks', *args)
+    generate('ks/models.ks', *args)
